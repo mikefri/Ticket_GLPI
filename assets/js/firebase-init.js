a48ffb0f9e5ed6166ad2ee7a59dff5539e25d858
Micfri <44413144+mikefri@users.noreply.github.com>
2026-01-30 18:02:26 +0100

    Update firebase-init.js

--- assets/js/firebase-init.js
+++ assets/js/firebase-init.js
@@ -2,13 +2,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-export const firebaseConfig = {
-  apiKey: "VOTRE_API_KEY",
-  authDomain: "VOTRE_PROJET.firebaseapp.com",
-  projectId: "VOTRE_PROJECT_ID",
-  storageBucket: "VOTRE_PROJECT_ID.appspot.com",
-  messagingSenderId: "VOTRE_SENDER_ID",
-  appId: "VOTRE_APP_ID"
+const firebaseConfig = {
+  apiKey: "AIzaSyBDjuTUFVR5Ncxg5ptIZXB-CKU2HPUYQKU",
+  authDomain: "serviceedglpi.firebaseapp.com",
+  databaseURL: "https://serviceedglpi-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "serviceedglpi",
+  storageBucket: "serviceedglpi.firebasestorage.app",
+  messagingSenderId: "972055143589",
+  appId: "1:972055143589:web:cac4bd60283f2160a1994c"
 };
 
 const app = initializeApp(firebaseConfig);
